f5f524e56415b5d001afc55044b739225d129714
Ronik <102733678+Ronikrr@users.noreply.github.com>
2025-01-27 15:44:41 +0530

    ronik10:09

--- src/components/common/sizechart.js
+++ src/components/common/sizechart.js
@@ -27,23 +27,24 @@ const sizeOptions = [
 
 
 const ageOptions = [
-  { value: "6 Month to 12 Month", label: "6 Month to 12 Month" },
-  { value: "1 Year to 2 Year", label: "1 Year to 2 Year" },
-  { value: "2 Year to 3 Year", label: "2 Year to 3 Year" },
-  { value: "3 Year to 4 Year", label: "3 Year to 4 Year" },
-  { value: "4 Year to 5 Year", label: "4 Year to 5 Year" },
-  { value: "5 Year to 6 Year", label: "5 Year to 6 Year" },
-  { value: "6 Year to 7 Year", label: "6 Year to 7 Year" },
-  { value: "7 Year to 8 Year", label: "7 Year to 8 Year" },
-  { value: "8 Year to 9 Year", label: "8 Year to 9 Year" },
-  { value: "9 Year to 10 Year", label: "9 Year to 10 Year" },
-  { value: "10 Year to 11 Year", label: "10 Year to 11 Year" },
-  { value: "11 Year to 12 Year", label: "11 Year to 12 Year" },
-  { value: "12 Year to 13 Year", label: "12 Year to 13 Year" },
-  { value: "13 Year to 14 Year", label: "13 Year to 14 Year" },
-  { value: "14 Year to 15 Year", label: "14 Year to 15 Year" },
-  { value: "15 Year to 16 Year", label: "15 Year to 16 Year" },
-  { value: "16 Year to 17 Year", label: "16 Year to 17 Year" },
+  // { value: "6 Month to 12 Month", label: "6 Month to 12 Month" },
+  // { value: "1 Year to 2 Year", label: "1 Year to 2 Year" },
+  // { value: "2 Year to 3 Year", label: "2 Year to 3 Year" },
+  // { value: "3 Year to 4 Year", label: "3 Year to 4 Year" },
+  // { value: "4 Year to 5 Year", label: "4 Year to 5 Year" },
+  // { value: "5 Year to 6 Year", label: "5 Year to 6 Year" },
+  // { value: "6 Year to 7 Year", label: "6 Year to 7 Year" },
+  // { value: "7 Year to 8 Year", label: "7 Year to 8 Year" },
+  // { value: "8 Year to 9 Year", label: "8 Year to 9 Year" },
+  // { value: "9 Year to 10 Year", label: "9 Year to 10 Year" },
+  // { value: "10 Year to 11 Year", label: "10 Year to 11 Year" },
+  // { value: "11 Year to 12 Year", label: "11 Year to 12 Year" },
+  // { value: "12 Year to 13 Year", label: "12 Year to 13 Year" },
+  // { value: "13 Year to 14 Year", label: "13 Year to 14 Year" },
+  // { value: "14 Year to 15 Year", label: "14 Year to 15 Year" },
+  // { value: "15 Year to 16 Year", label: "15 Year to 16 Year" },
+  // { value: "16 Year to 17 Year", label: "16 Year to 17 Year" },
+  { value: "S", label: "S" },
 ]
 
 const INIT_FIELD = [
@@ -114,10 +115,10 @@ const Sizechart = () => {
   return (
     <>
       {loading && <Spinner />}
-      <div className="container mx-auto p-4 sm:p-6 lg:p-8">
-        <h2 className="text-3xl font-semibold mb-2">Select Measurements</h2>
+      <div className="container p-4 mx-auto sm:p-6 lg:p-8">
+        <h2 className="mb-2 text-3xl font-semibold">Select Measurements</h2>
         <div className="mb-2">
-          <label className="block text-md font-medium mb-0">Product</label>
+          <label className="block mb-0 font-medium text-md">Product</label>
           <Select
             options={productsOptions}
             value={productsOptions.find((opt) => opt.value === productsId)}
@@ -128,10 +129,10 @@ const Sizechart = () => {
           />
         </div>
         {fields.map((field, index) => (
-          <div key={index} className="bg-white shadow-md rounded-lg p-6 mb-6">
-            <div className="grid lg:grid-cols-3 gap-5">
-              <div className="mb-2">
-                <label className="block text-md font-medium mb-0">Size</label>
+          <div key={index} className="p-6 mb-6 bg-white rounded-lg shadow-md">
+            <div className="grid gap-5 lg:grid-cols-3">
+              {/* <div className="mb-2">
+                <label className="block mb-0 font-medium text-md">Size</label>
                 <Select
                   options={sizeOptions}
                   value={sizeOptions.find((opt) => opt.value === field.size)}
@@ -140,9 +141,9 @@ const Sizechart = () => {
                   classNamePrefix="react-select"
                   styles={{ zIndex: 10 }}
                 />
-              </div>
+              </div> */}
               <div className="mb-2">
-                <label className="block text-md font-medium mb-0">Age</label>
+                <label className="block mb-0 font-medium text-md">Age</label>
                 <Select
                   options={ageOptions}
                   value={ageOptions.find((opt) => opt.value === field.age)}
@@ -153,92 +154,92 @@ const Sizechart = () => {
                 />
               </div>
               <div className="mb-2">
-                <label className="block text-md font-medium mb-0">Chest (in inches)</label>
+                <label className="block mb-0 font-medium text-md">Chest (in inches)</label>
                 <input
                   type="text"
                   value={field.chest}
                   onChange={(e) => handleChange(index, "chest", e.target.value)}
-                  className="border rounded-md p-2 w-full"
+                  className="w-full p-2 border rounded-md"
                   style={{ background: "#fff", border: "1px solid #ccc", zIndex: "0" }}
                 />
               </div>
               <div className="mb-2">
-                <label className="block text-md font-medium mb-0">Dress Length (in inches)</label>
+                <label className="block mb-0 font-medium text-md">Dress Length (in inches)</label>
                 <input
                   type="text"
                   value={field.dressLength}
                   onChange={(e) => handleChange(index, "dressLength", e.target.value)}
-                  className="border rounded-md p-2 w-full"
+                  className="w-full p-2 border rounded-md"
                   style={{ background: "#fff", border: "1px solid #ccc", zIndex: "0" }}
                 />
               </div>
               <div className="mb-2">
-                <label className="block text-md font-medium mb-0">Waist (in inches)</label>
+                <label className="block mb-0 font-medium text-md">Waist (in inches)</label>
                 <input
                   type="text"
                   value={field.waist}
                   onChange={(e) => handleChange(index, "waist", e.target.value)}
-                  className="border rounded-md p-2 w-full"
+                  className="w-full p-2 border rounded-md"
                   style={{ background: "#fff", border: "1px solid #ccc", zIndex: "0" }}
                 />
               </div>
               <div className="mb-2">
-                <label className="block text-md font-medium mb-0">Short Length (in inches)</label>
+                <label className="block mb-0 font-medium text-md">Short Length (in inches)</label>
                 <input
                   type="text"
                   value={field.shortLength}
                   onChange={(e) => handleChange(index, "shortLength", e.target.value)}
-                  className="border rounded-md p-2 w-full"
+                  className="w-full p-2 border rounded-md"
                   style={{ background: "#fff", border: "1px solid #ccc", zIndex: "0" }}
                 />
               </div>
               <div className="mb-2">
-                <label className="block text-md font-medium mb-0">Frok Length (in inches)</label>
+                <label className="block mb-0 font-medium text-md">Frok Length (in inches)</label>
                 <input
                   type="text"
                   value={field.frokLength}
                   onChange={(e) => handleChange(index, "frokLength", e.target.value)}
-                  className="border rounded-md p-2 w-full"
+                  className="w-full p-2 border rounded-md"
                   style={{ background: "#fff", border: "1px solid #ccc", zIndex: "0" }}
                 />
               </div>
               <div className="mb-2">
-                <label className="block text-md font-medium mb-0">Top Length(in inches)</label>
+                <label className="block mb-0 font-medium text-md">Top Length(in inches)</label>
                 <input
                   type="text"
                   value={field.topLength}
                   onChange={(e) => handleChange(index, "topLength", e.target.value)}
-                  className="border rounded-md p-2 w-full"
+                  className="w-full p-2 border rounded-md"
                   style={{ background: "#fff", border: "1px solid #ccc", zIndex: "0" }}
                 />
               </div>
               <div className="mb-2">
-                <label className="block text-md font-medium mb-0">Skirt Length (in inches)</label>
+                <label className="block mb-0 font-medium text-md">Skirt Length (in inches)</label>
                 <input
                   type="text"
                   value={field.skirtLength}
                   onChange={(e) => handleChange(index, "skirtLength", e.target.value)}
-                  className="border rounded-md p-2 w-full"
+                  className="w-full p-2 border rounded-md"
                   style={{ background: "#fff", border: "1px solid #ccc", zIndex: "0" }}
                 />
               </div>
               <div className="mb-2">
-                <label className="block text-md font-medium mb-0">Blouse Length (in inches)</label>
+                <label className="block mb-0 font-medium text-md">Blouse Length (in inches)</label>
                 <input
                   type="text"
                   value={field.blouseLength}
                   onChange={(e) => handleChange(index, "blouseLength", e.target.value)}
-                  className="border rounded-md p-2 w-full"
+                  className="w-full p-2 border rounded-md"
                   style={{ background: "#fff", border: "1px solid #ccc", zIndex: "0" }}
                 />
               </div>
               <div className="mb-2">
-                <label className="block text-md font-medium mb-0">bottom Length (in inches)</label>
+                <label className="block mb-0 font-medium text-md">bottom Length (in inches)</label>
                 <input
                   type="text"
                   value={field.bottomLength}
                   onChange={(e) => handleChange(index, "bottomLength", e.target.value)}
-                  className="border rounded-md p-2 w-full"
+                  className="w-full p-2 border rounded-md"
                   style={{ background: "#fff", border: "1px solid #ccc", zIndex: "0" }}
                 />
               </div>
@@ -248,7 +249,7 @@ const Sizechart = () => {
 
         <button
           onClick={addField}
-          className="bg-green-600 text-white font-semibold py-2 px-4 rounded hover:bg-green-700"
+          className="px-4 py-2 font-semibold text-white bg-green-600 rounded hover:bg-green-700"
           style={{ background: "#82ca9d", padding: "8px 16px", color: "#fff", marginTop: "20px" }}
         >
           Add Field
@@ -256,7 +257,7 @@ const Sizechart = () => {
 
         <button
           onClick={handleSubmit}
-          className="bg-green-600 text-white font-semibold py-2 px-4 rounded hover:bg-green-700"
+          className="px-4 py-2 font-semibold text-white bg-green-600 rounded hover:bg-green-700"
           style={{ background: "#e5097f", padding: "8px 16px", color: "#fff", marginLeft: "10px" }}
         >
           Submit
