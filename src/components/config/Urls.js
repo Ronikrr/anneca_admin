b56b4354aad14c37959847dbcec87b20e9f2c9d4
Ronik <102733678+Ronikrr@users.noreply.github.com>
2025-01-28 14:50:55 +0530

    ronik10:09

--- src/components/config/Urls.js
+++ src/components/config/Urls.js
@@ -1,2 +1,2 @@
-export const BASE_URL = "https://anneca-backend.onrender.com/api/v1/";
+export const BASE_URL = "https://anneca-backend-sepia.vercel.app/api/v1/";
 // export const BASE_URL = "http://localhost:8000/api/v1/"
